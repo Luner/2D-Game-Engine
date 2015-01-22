199d3c454e427e9e84bf0539442fc9ee12eae8fb
Luner <jonathan@luner.net>
2015-01-22 11:43:22 -0800

    This adds the first Main file in an attempt to test the arcanist functionality
    Summary: This is a test
    
    Test Plan: Manual
    
    Subscribers: Luner
    
    Differential Revision: https://game-engine-test.phoreplay.com/D1

--- Game Engine/src/Main.java
+++ Game Engine/src/Main.java
@@ -2,5 +2,6 @@
 public class Main {
 	public static void main(String[] args){
 		System.out.println("This file is a test one that should be used to ensure that this stuff is working.");
+		System.out.println("This adds a second println file");
 	}
 }
