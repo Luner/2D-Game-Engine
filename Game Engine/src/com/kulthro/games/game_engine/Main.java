7541605255cc36c9f800a234e6add152eb8f4dc6
Joey Iny <joeyainy@gmail.com>
2015-01-24 16:57:42 -0800

    Set up LWJGL Drawing
    Level background is now being drawn.
    Added Width and Height variables.
    Added more drawing functions.
    Screen clears every tick.

--- Game Engine/src/com/kulthro/games/game_engine/Main.java
+++ Game Engine/src/com/kulthro/games/game_engine/Main.java
@@ -2,17 +2,29 @@ package com.kulthro.games.game_engine;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import static org.lwjgl.opengl.GL11.*;
 
 public class Main {
+	
+	private final static int WIDTH = 800;
+	private final static int HEIGHT = 600;
+	
 	public static void main(String[] args) {
 		
-		//Creates Display of 800x600
+		//Creates Display of WIDTH x HEIGHT
 		try {
-			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.setDisplayMode(new DisplayMode(WIDTH,HEIGHT));
 			Display.create();
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 		}
+		
+		// Sets up LWJGL with OpenGL.
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, WIDTH, 0, HEIGHT, 1, -1);
+		glMatrixMode(GL_MODELVIEW);
 		
 		while(!Display.isCloseRequested()) {
 			
@@ -31,10 +43,37 @@ public class Main {
 	
 	/* Rendering functions*/
 	public static void draw() {
+		clearScreen();
+		
 		drawBackground();
+		drawBlocks();
+		drawEntities();
+	}
+	
+	public static void clearScreen() {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 	}
 	
 	public static void drawBackground() {
+		glBegin(GL_QUADS);
+		
+		glColor3f(0.1f,0.4f,0.8f);
+		
+		glVertex2f(0,HEIGHT);
+		glVertex2f(WIDTH,HEIGHT);
+		
+		glColor3f(0.3f,0.8f,1f);
+		
+		glVertex2f(WIDTH,0);
+		glVertex2f(0,0);
+		glEnd();
+	}
+	
+	public static void drawBlocks() {
+		
+	}
+	
+	public static void drawEntities() {
 		
 	}
 	
