43113c30a618a750679b2562e7daace8f2ebcb83
Joey Iny <joeyainy@gmail.com>
2015-01-25 19:01:57 -0800

    Added spaces in method headers

--- Game Engine/src/com/kulthro/games/game_engine/Vector2f.java
+++ Game Engine/src/com/kulthro/games/game_engine/Vector2f.java
@@ -5,32 +5,32 @@ public class Vector2f {
 	private float x;
 	private float y;
 	
-	public Vector2f(float x, float y){
+	public Vector2f(float x, float y) {
 		this.x = x;
 		this.y = y;
 	}
 	
-	public Vector2f add(Vector2f v){
+	public Vector2f add(Vector2f v) {
 		return new Vector2f(this.x + v.getX(), this.y + v.getY());
 	}
 	
-	public Vector2f scale(float v){
+	public Vector2f scale(float v) {
 		return new Vector2f(this.x * v, this.y * v);
 	}
 	
-	public void setX(float x){
+	public void setX(float x) {
 		this.x = x;
 	}
 	
-	public void setY(float y){
+	public void setY(float y) {
 		this.y = y;
 	}
 	
-	public float getX(){
+	public float getX() {
 		return this.x;
 	}
 	
-	public float getY(){
+	public float getY() {
 		return this.y;
 	}
 }
