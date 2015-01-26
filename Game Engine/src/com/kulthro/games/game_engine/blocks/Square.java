762969e389add4941f9154e1f62b39e8a1299243
Luner <jonathan@luner.net>
2015-01-26 10:33:19 -0800

    Changes Square with Quad
    I am thinking of having this class extend Entity and then having the
    things like mobs extend these different shapes

--- Game Engine/src/com/kulthro/games/game_engine/blocks/Square.java
+++
@@ -1,9 +0,0 @@
-package com.kulthro.games.game_engine.blocks;
-
-public class Square {
-	//collision with a circle bounce off opposite of the direction from the circle
-	private float width;
-	private float height;
-	
-	
-}
