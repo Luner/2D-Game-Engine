af026844425aced9eff46638542d002e7802954c
Joey Iny <joeyainy@gmail.com>
2015-01-25 20:32:46 -0800

    Mobs now have textures

--- Game Engine/src/com/kulthro/games/game_engine/entities/Mob.java
+++ Game Engine/src/com/kulthro/games/game_engine/entities/Mob.java
@@ -1,6 +1,10 @@
 package com.kulthro.games.game_engine.entities;
 
+import org.newdawn.slick.opengl.Texture;
+
 public abstract class Mob extends Entity {
+	
+	private Texture tex;
 
 	private int health;
 	
@@ -10,6 +14,14 @@ public abstract class Mob extends Entity {
 	
 	public Mob(float x, float y) {
 		super(x, y);
+	}
+	
+	public Texture getTexture() {
+		return tex;
+	}
+	
+	public void setTexture(Texture texture) {
+		this.tex = texture;
 	}
 	
 }
