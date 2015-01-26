17aadff18ac5ee8b34c390003977252600331f8e
Luner <jonathan@luner.net>
2015-01-25 19:23:34 -0800

    Render Class

---
+++ Game Engine/src/com/kulthro/games/game_engine/Render.java
@@ -0,0 +1,27 @@
+package com.kulthro.games.game_engine;
+
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glBindTexture;
+import static org.lwjgl.opengl.GL11.glEnd;
+import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.opengl.Texture;
+
+public class Render {
+	public static void renderQuad(float leftX,float bottomY,float rightX,float topY, Texture tex){
+		tex.bind();
+		glBegin(GL_QUADS);
+		GL11.glColor3f(1.0f, 1.0f, 1.0f);
+		GL11.glTexCoord2f(0.0f,1.0f);
+		GL11.glVertex2f(leftX, bottomY);    // Bottom Left Of The Quad
+		GL11.glTexCoord2f(1.0f,1.0f);
+		GL11.glVertex2f(rightX, bottomY);   // Bottom Right Of The Quad 
+		GL11.glTexCoord2f(1.0f,0.0f);
+		GL11.glVertex2f(rightX, topY);		// Top Right Of The Quad 
+		GL11.glTexCoord2f(0.0f,0.0f);
+		GL11.glVertex2f(leftX, topY);  		// Top Left Of The Quad 
+		glEnd();
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+}
--- Game Engine/src/com/kulthro/games/game_engine/menu/Menu.java
+++ Game Engine/src/com/kulthro/games/game_engine/menu/Menu.java
@@ -1,7 +1,10 @@
 package com.kulthro.games.game_engine.menu;
 
+import com.kulthro.games.game_engine.Render;
 import com.kulthro.games.game_engine.files.ImageReader;
+
 import static org.lwjgl.opengl.GL11.*;
+
 import org.lwjgl.opengl.GL11;
 
 public class Menu { 
@@ -49,19 +52,7 @@ public class Menu {
 	}
 
 	private void renderButton(SquareButton button){
-		
+		Render.renderQuad(button.leftX, button.bottomY, button.rightX, button.topY, button.getTexture());
 		button.getTexture().bind();
-		glBegin(GL_QUADS);
-		GL11.glColor3f(1.0f, 1.0f, 1.0f);
-		GL11.glTexCoord2f(0.0f,1.0f);
-		GL11.glVertex2f(button.leftX, button.bottomY);        	// Bottom Left Of The Quad
-		GL11.glTexCoord2f(1.0f,1.0f);
-		GL11.glVertex2f(button.rightX, button.bottomY);   	// Bottom Right Of The Quad 
-		GL11.glTexCoord2f(1.0f,0.0f);
-		GL11.glVertex2f(button.rightX, button.topY);	// Top Right Of The Quad 
-		GL11.glTexCoord2f(0.0f,0.0f);
-		GL11.glVertex2f(button.leftX, button.topY);  		// Top Left Of The Quad 
-		glEnd();
-		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 }
