b0d774851cd91c660b72af31c5e7d441ab6cc52e
Joey Iny <joeyainy@gmail.com>
2015-01-27 20:34:27 -0800

    Entities and Blocks are now both damageable

--- Game Engine/src/com/kulthro/games/game_engine/blocks/Quad.java
+++ Game Engine/src/com/kulthro/games/game_engine/blocks/Quad.java
@@ -2,56 +2,50 @@ package com.kulthro.games.game_engine.blocks;
 
 import com.kulthro.games.game_engine.Vector2f;
 
-public class Quad {
-	//collision with a circle bounce off opposite of the direction from the circle
-	private Vector2f position;
-	private Vector2f direction;
-	private float width;
-	private float height;
+public class Quad extends Block {
 	
-	public Quad(Vector2f position, float width, float height){
-		this.position = position;
-		this.width = width;
-		this.height = height;
+	public Quad(Vector2f position, float width, float height) {
+		super(position, width, height);
 	}
 	
-	public void move(){
+	public void move() {
 		this.position = this.position.add(direction);
 	}
 	
-	public Vector2f getDirection(){
+	public Vector2f getDirection() {
 		return direction;
 	}
 	
-	public Vector2f getPosition(){
+	public Vector2f getPosition() {
 		return position;
 	}
 	
-	public float getWidth(){
+	public float getWidth() {
 		return width;
 	}
 	
-	public float getHeight(){
+	public float getHeight() {
 		return height;
 	}
 	 
-	public void setDirection(Vector2f direction){
+	public void setDirection(Vector2f direction) {
 		this.direction = direction;
 	}
 	
-	public void setPosition(Vector2f position){
+	public void setPosition(Vector2f position) {
 		this.position = position;
 	}
 	
-	public void setHeight(float height){
+	public void setHeight(float height) {
 		this.height = height;
 	}
 	
-	public void setWidth(float width){
+	public void setWidth(float width) {
 		this.width = width;
 	}
 	
-	public float area(){
+	public float area() {
 		return this.width * this.height;
 	}
+
 }
---
+++ Game Engine/src/com/kulthro/games/game_engine/levels/Level.java
@@ -0,0 +1,15 @@
+package com.kulthro.games.game_engine.levels;
+
+import java.util.ArrayList;
+
+import com.kulthro.games.game_engine.blocks.Block;
+
+public class Level {
+	
+	public ArrayList<Block> blocks;
+	
+	public Level() {
+		
+	}
+	
+}
